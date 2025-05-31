e9bd05ee79e32319c728e4437939c7d19f2dc690
Lucas Mendes <luckajw@gmail.com>
2025-05-30 21:37:28 -0300

    estabelecida a requisição na api, e eventos de filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable max-len */
-import React from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +6,30 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+
+const API_TODOS = 'http://localhost:5173/api/todos.json';
+const API_USER = 'http://localhost:5173/api/users.json';
+
+fetch(API_USER).then(response => response.json());
 
 export const App: React.FC = () => {
+  function getAll(): Promise<Todo[]> {
+    return fetch(API_TODOS).then(response => response.json());
+  }
+
+  function getAllActive() {
+    return getAll().then(todos =>
+      todos.filter(todo => todo.completed === false),
+    );
+  }
+
+  function getAllCompleted() {
+    return getAll().then(todos =>
+      todos.filter(todo => todo.completed === true),
+    );
+  }
+
   return (
     <>
       <div className="section">
@@ -17,7 +38,11 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onClickAll={() => getAll}
+                onClickHandlerActive={() => getAllActive}
+                onClickHandlerCompleted={() => getAllCompleted}
+              />
             </div>
 
             <div className="block">
